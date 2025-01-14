5b76458a2e9050c3ab8c4a192203308b73e7d4b2
NoTbre4d <sebaspedrozo.23@hotmail.com>
2025-01-14 15:22:54 -0500

    pruebas

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,24 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:3000', 'http://localhost:1337', 'http://127.0.0.1:3000'],
+      headers: [
+        'Content-Type',
+        'Authorization',
+        'X-Frame-Options',
+        'Access-Control-Allow-Headers',
+        'Origin',
+        'Accept',
+        'Pragma',
+        'Cache-Control'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS']
+    }
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
