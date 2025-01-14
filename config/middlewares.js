8e137f8997efc6b9614a1f27a7691e7aeb251421
NoTbre4d <sebaspedrozo.23@hotmail.com>
2025-01-14 16:04:45 -0500

    arreglos

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,40 +1,8 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'http://*', 'https://*'],
-          'img-src': ["'self'", 'data:', 'blob:', 'http://*', 'https://*'],
-          'media-src': ["'self'", 'data:', 'blob:', 'http://*', 'https://*'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      origin: ['http://localhost:3000', 'http://localhost:1337', 'http://127.0.0.1:3000'],
-      headers: [
-        'Content-Type',
-        'Authorization',
-        'Origin',
-        'Accept',
-        'Cache-Control',
-        'Pragma',
-        'X-Requested-With'
-      ],
-      exposedHeaders: ['Content-Range', 'X-Total-Count'],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
-      credentials: true,
-      maxAge: 86400
-    }
-  },
+  'strapi::security',
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
